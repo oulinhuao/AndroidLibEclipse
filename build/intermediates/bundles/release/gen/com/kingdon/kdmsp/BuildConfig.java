8219698b9ed98a534fe4da539d5a74bb622eec23
oulinhuao <397013181@qq.com>
2017-02-13 14:26:31 +0800

    首导入

---
+++ build/intermediates/bundles/release/gen/com/kingdon/kdmsp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.kingdon.kdmsp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/proj/androidlib/model/DownFile.java
@@ -0,0 +1,22 @@
+package com.proj.androidlib.model;
+
+/**
+ * 下载文件
+ *
+ * @author Wayne
+ *
+ */
+public class DownFile {
+    /**
+     * 文件名称
+     */
+    public String FileName;
+    /**
+     * 网络地址
+     */
+    public String Url;
+    /**
+     * 本地地址
+     */
+    public String LocalPath;
+}
